70fa30d25734bb024155fbd3bc67e51b44f4f43e
Arif Ahmad Alfian <49640973+arifahmadalfian@users.noreply.github.com>
2021-10-16 23:40:51 +0700

    Utils converter exstensions function epoch to date

--- app/src/main/java/com/arifahmadalfian/openweathermap/utils/Utils.kt
+++ app/src/main/java/com/arifahmadalfian/openweathermap/utils/Utils.kt
@@ -5,9 +5,23 @@ import android.text.format.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
-fun Long.epochToDateTime(): String {
+/**
+ * ctrl + Q
+ * @param style when value is
+ * 1 -> 2021-05-28 12:00
+ * 2 -> Selasa, 17 Oktober 2021 12:00
+ * 3 -> 12:00 17-Okt-2021
+ * 4 -> 17/08/2021
+ */
+fun Long.epochToDateTime(style: Int): String {
     val date = Date(this * 1000L)
-    val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSSSS")
+    val fmt: String = when (style) {
+       1 -> "yyyy-MM-dd HH:mm" 
+       2 -> "EEEE, d MMM yyyy HH:mm"
+       3 -> "HH:mm dd-MM-yyyy" 
+       4 -> "dd/MM/yyyy"
+    }
+    val sdf = SimpleDateFormat(fmt)
     return sdf.format(date)
 }
 
